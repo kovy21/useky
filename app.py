5cb92685971924615ddf6630ceb30adc92c4b260
Peter Kováč <47066564+kovy21@users.noreply.github.com>
2022-10-14 17:24:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_the_map():
-    return render_template('useky_all_shp.html')
+    return render_template('the_map.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
